4ffb82d70e7cbdcf26b2a238f1bf9341e1ae2375
Flavio Rocha <flavio_271295@hotmail.com>
2021-01-02 20:38:40 -0300

    Updates

---
+++ precache-manifest.a44c17e4f655772ba41ddb13fdb9df17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4eda31020c2d2b7f3c54f5525def02",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "d298103663ef3d6443aa",
+    "url": "/netflix-clone/static/css/main.5026592d.chunk.css"
+  },
+  {
+    "revision": "3d68d3192c615fc10228",
+    "url": "/netflix-clone/static/js/2.564effa5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netflix-clone/static/js/2.564effa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d298103663ef3d6443aa",
+    "url": "/netflix-clone/static/js/main.08c12c98.chunk.js"
+  },
+  {
+    "revision": "c97f3b5bd6ec61c5260d",
+    "url": "/netflix-clone/static/js/runtime-main.4ba779e9.js"
+  }
+]);
